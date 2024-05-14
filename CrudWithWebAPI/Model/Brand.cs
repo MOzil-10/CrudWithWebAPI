96b49bbd0eb4c1d4abdfdd86eef3c89548f03478
Mukosi Budeli <mukosi@digitalsolutionfoundry.co.za>
2024-05-14 12:28:21 +0200

    Add project files.

---
+++ CrudWithWebAPI/Model/Brand.cs
@@ -0,0 +1,10 @@
+﻿namespace CrudWithWebAPI.Model
+{
+    public class Brand
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public int IsActive { get; set; }
+    }
+}
